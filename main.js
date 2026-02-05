532d21e6c299ecf68a8ad9383800df6c30959256
LottaCinthio <jolo25uh@student.ju.se>
2026-02-05 14:28:36 +0100

    updated main.js

--- main.js
+++ main.js
@@ -2,64 +2,66 @@ require([
   "esri/Map",
   "esri/views/SceneView",
   "esri/layers/GeoJSONLayer",
-  "esri/widgets/Search",
   "esri/widgets/LayerList"
-], function (Map, SceneView, GeoJSONLayer, Search, LayerList) {
+], function(Map, SceneView, GeoJSONLayer, LayerList) {
 
+  // 1. THE LIST: The files you want to show
   const analysisFiles = [
-   { 
+    { 
       name: "Jönköping Buildings", 
       file: "buildings.geojson", 
-      height: 15, // Tall buildings
-      color: [255, 255, 255, 0.9] // White
+      height: 15, 
+      color: [255, 255, 255, 0.9] 
     },
     { 
       name: "Parking Spots", 
       file: "Parkingspots.geojson", 
-      height: 0, // Flat on the ground
-      color: [255, 255, 0, 0.7] // Yellow (so they stand out)
+      height: 0, 
+      color: [0, 197, 255, 0.6] 
     },
     { 
       name: "Playgrounds", 
       file: "Playgrounds.geojson", 
       height: 0, 
-      color: [76, 230, 0] // Bright Green
+      color: [76, 230, 0] 
     }
   ];
 
   const map = new Map({
-    basemap: "gray-vector",
+    basemap: "topo-vector",
     ground: "world-elevation"
   });
 
-aanalysisFiles.forEach(info => {
+  const view = new SceneView({
+    container: "viewDiv",
+    map: map,
+    camera: {
+      position: [14.16, 57.78, 2000],
+      tilt: 45
+    }
+  });
+
+  // 2. THE LOOP: This handles adding the layers
+  analysisFiles.forEach(info => {
     let layerRenderer;
 
     if (info.name === "Playgrounds") {
-      // Logic for Playgrounds (Point data)
+      // Logic for Points (Playgrounds) using a simple circular marker
       layerRenderer = {
         type: "simple",
         symbol: {
-          type: "picture-marker",
-          // Standard ArcGIS Playground Icon URL
-          url: "https://static.arcgis.com/images/Symbols/OutdoorRecreation/Playground.png",
-          width: "24px",
-          height: "24px"
+          type: "simple-marker", 
+          style: "circle",
+          color: [76, 230, 0],
+          size: "12px",
+          outline: { color: "white", width: 1 }
         }
       };
     } else {
-      // Logic for Buildings and Parking (Polygon data)
+      // Logic for Polygons (Buildings and Parking)
       const symbolLayer = info.height > 0 
-        ? { 
-            type: "extrude", 
-            size: info.height, 
-            material: { color: info.color } 
-          }
-        : { 
-            type: "fill", 
-            material: { color: info.color }, 
-            outline: { color: [255, 255, 255, 0.4], size: 1 } 
-          };
+        ? { type: "extrude", size: info.height, material: { color: info.color } }
+        : { type: "fill", material: { color: info.color }, outline: { color: [255, 255, 255, 0.4], size: 1 } };
       
       layerRenderer = {
         type: "simple",
@@ -73,32 +75,17 @@ aanalysisFiles.forEach(info => {
     const layer = new GeoJSONLayer({
       url: "./data/" + info.file,
       title: info.name,
-      // Important: "absolute-height" or "on-the-ground" ensures points aren't hidden
-      elevationInfo: { 
-        mode: "on-the-ground" 
-      },
+      elevationInfo: { mode: "on-the-ground" },
       renderer: layerRenderer
     });
     map.add(layer);
   });
-  
-  const view = new SceneView({
-    container: "viewDiv",
-    map: map,
-    camera: {
-      position: {
-        longitude: 14.274, // Centered on your data's coordinates
-        latitude: 57.797, 
-        z: 1000
-      },
-      tilt: 50
-    }
+
+  view.when(() => {
+    const layerList = new LayerList({ view: view });
+    view.ui.add(layerList, "bottom-left");
   });
-
-  view.ui.add(new Search({ view: view }), "top-right");
-  view.ui.add(new LayerList({ view: view }), "bottom-left");
 });
-
 //name: "Building Footprints", 
 //    file: "buildings.geojson", 
 //     height: 25, 
