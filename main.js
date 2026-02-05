0bc1c965bb35cb6bb7a92e6e3a8134d790ed088d
LottaCinthio <jolo25uh@student.ju.se>
2026-02-05 12:48:52 +0100

    changes fro buildings

--- main.js
+++ main.js
@@ -6,66 +6,54 @@ require([
   "esri/widgets/LayerList"
 ], function (Map, SceneView, GeoJSONLayer, Search, LayerList) {
 
-  // 1. THE LIST: Corrected filename and properties
   const analysisFiles = [
     { 
       name: "Jönköping Buildings", 
       file: "buildings.geojson", 
-      height: 30, 
-      color: [0, 120, 255, 0.8] 
+      color: [255, 255, 255, 0.9] // Professional White
     }
   ];
 
-  // 2. CREATE THE MAP
   const map = new Map({
     basemap: "gray-vector",
-    ground: "world-elevation" // Enables 3D terrain
+    ground: "world-elevation"
   });
 
-  // 3. THE LOOP: Added Elevation logic to prevent "Underground" buildings
   analysisFiles.forEach(info => {
     const layer = new GeoJSONLayer({
       url: "./data/" + info.file,
       title: info.name,
-      // MANDATORY: Sit buildings on top of the Jönköping hills
       elevationInfo: {
-        mode: "relative-to-ground",
-        offset: 0
+        mode: "relative-to-ground"
       },
       renderer: {
         type: "simple",
         symbol: {
           type: "polygon-3d",
           symbolLayers: [{
-            type: "extrude", // This creates the 3D volume
-            size: info.height,
+            type: "extrude",
+            size: 15, // FORCED HEIGHT: Since your file has no 'height' property
             material: { color: info.color }
           }]
         }
-      },
-      popupTemplate: {
-        title: "{name}",
-        content: "Building Height: {height} m"
       }
     });
     map.add(layer);
   });
 
-  // 4. THE VIEW: Moved camera from Stockholm to Jönköping
   const view = new SceneView({
     container: "viewDiv",
     map: map,
     camera: {
       position: {
-        longitude: 14.1618, // Jönköping center
-        latitude: 57.7826, 
-        z: 1500
+        longitude: 14.274, // Centered on your data's coordinates
+        latitude: 57.797, 
+        z: 1000
       },
-      tilt: 45
+      tilt: 50
     }
   });
 
-  // 5. WIDGETS
   view.ui.add(new Search({ view: view }), "top-right");
   view.ui.add(new LayerList({ view: view }), "bottom-left");
 });
