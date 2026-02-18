f6d120f6aaf3696e6b555e1d2b92befcea4e988a
LottaCithio <jolo25uh@student.ju.se>
2026-02-18 09:22:34 +0100

    walking zones

--- main.js
+++ main.js
@@ -7,11 +7,18 @@ require([
 ], function (Map, SceneView, GeoJSONLayer, Search, LayerList) {
 
   const analysisFiles = [
+    // 1. UPDATED FILENAME: Your Service Area "blobs"
+    { 
+      name: "Walking Accessibility to Schools", 
+      file: "Walk_zones_to_school.geojson", 
+      type: "analysis" 
+    },
+    // 2. BUILDINGS: Using Join_Count to highlight schools
     { 
       name: "Jönköping Buildings", 
       file: "buildings.geojson", 
       height: 15, 
-      color: [255, 255, 255, 0.9] 
+      type: "building" 
     },
     { 
       name: "Parking Spots", 
@@ -22,14 +29,12 @@ require([
     { 
       name: "Playgrounds", 
       file: "Playgrounds.geojson", 
-      height: 0, 
-      color: [76, 230, 0]
+      type: "icon" 
     },
     { 
       name: "Bus Stops", 
       file: "Busstops.geojson", 
-      height: 0, 
-      color: [0, 92, 230]
+      type: "icon" 
     }
   ];
 
@@ -41,72 +46,61 @@ require([
   analysisFiles.forEach(info => {
     let renderer;
 
-    // POINT LAYERS: Bus stops & playgrounds
-    if (info.name === "Bus Stops" || info.name === "Playgrounds") {
+    // RENDERER FOR WALK ZONES: 5, 10, 15 minute isochrones
+    if (info.type === "analysis") {
+      renderer = {
+        type: "unique-value",
+        field: "ToBreak", 
+        uniqueValueInfos: [
+          { value: 5, symbol: { type: "simple-fill", color: [46, 204, 113, 0.5], outline: { width: 0 } } },
+          { value: 10, symbol: { type: "simple-fill", color: [241, 196, 15, 0.4], outline: { width: 0 } } },
+          { value: 15, symbol: { type: "simple-fill", color: [230, 126, 34, 0.3], outline: { width: 0 } } }
+        ]
+      };
+    } 
+    
+    // RENDERER FOR 3D BUILDINGS: Extrusion and School Highlighting
+    else if (info.type === "building") {
+      renderer = {
+        type: "unique-value",
+        field: "Join_Count", 
+        defaultSymbol: { 
+          type: "polygon-3d", 
+          symbolLayers: [{ type: "extrude", size: info.height, material: { color: [255, 255, 255, 0.9] } }] 
+        },
+        uniqueValueInfos: [
+          { 
+            value: 1, 
+            symbol: { 
+              type: "polygon-3d", 
+              symbolLayers: [{ type: "extrude", size: info.height + 5, material: { color: "red" } }] 
+            } 
+          }
+        ]
+      };
+    }
 
-      const iconUrl =
-        info.name === "Bus Stops"
-          ? "./icons/bus.svg"
-          : "./icons/playground.svg";
-
+    // RENDERER FOR ICONS: Bus stops & playgrounds
+    else if (info.type === "icon") {
+      const iconUrl = info.name === "Bus Stops" ? "./icons/bus.svg" : "./icons/playground.svg";
       renderer = {
         type: "simple",
         symbol: {
           type: "point-3d",
-          symbolLayers: [{
-            type: "icon",
-            resource: { href: iconUrl },
-
-            // Base size in pixels
-            size: 18,
-
-            // Scale with zoom
-            sizeStops: [
-              { distance: 2000, size: 10 },
-              { distance: 1000, size: 16 },
-              { distance: 500,  size: 24 },
-              { distance: 200,  size: 36 }
-            ],
-
-            outline: {
-              color: "white",
-              size: 1
-            }
-          }]
-        }
-      };
-
-    } else {
-      // POLYGON LAYERS: Buildings & parking
-      const symbolLayer = info.height > 0 
-        ? {
-            type: "extrude",
-            size: info.height,
-            material: { color: info.color }
-          }
-        : {
-            type: "fill",
-            material: { color: info.color },
-            outline: { color: [255, 255, 255, 0.4], size: 1 }
-          };
-
-      renderer = {
-        type: "simple",
-        symbol: {
-          type: "polygon-3d",
-          symbolLayers: [symbolLayer]
+          symbolLayers: [{ type: "icon", resource: { href: iconUrl }, size: 18 }]
         }
       };
     }
 
     const layer = new GeoJSONLayer({
+      // Ensure your data folder path is correct
       url: "./data/" + info.file + "?v=" + new Date().getTime(),
       title: info.name,
-      elevationInfo: {
-        mode: "relative-to-ground",
-        offset: 1
-      },
-      renderer: renderer
+      renderer: renderer,
+      elevationInfo: { 
+        mode: "relative-to-ground", 
+        offset: 1 
+      }
     });
 
     map.add(layer);
@@ -115,13 +109,9 @@ require([
   const view = new SceneView({
     container: "viewDiv",
     map: map,
-    camera: {
-      position: {
-        x: 14.274,
-        y: 57.797,
-        z: 1000
-      },
-      tilt: 50
+    camera: { 
+      position: { x: 14.242, y: 57.782, z: 1500 }, 
+      tilt: 50 
     }
   });
 
