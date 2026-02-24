b275aeec490dc85db9c22ab9290dfc45beb6fb23
LottaCinthio <jolo25uh@student.ju.se>
2026-02-24 14:58:38 +0100

    up

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ require([
     let renderer;
     let popupTemplate = null;
 
+    // --- RENDERERS ---
     if (info.type === "health-walk" || info.type === "walk") {
       const colors = info.type === "health-walk" ? [[52,152,219,0.6],[155,89,182,0.5],[44,62,80,0.4]] : [[46,204,113,0.5],[241,196,15,0.4],[230,126,34,0.3]];
       renderer = { type: "unique-value", field: "ToBreak", uniqueValueInfos: [{ value: 5, symbol: { type: "simple-fill", color: colors[0], outline: { width: 0 } } }, { value: 10, symbol: { type: "simple-fill", color: colors[1], outline: { width: 0 } } }, { value: 15, symbol: { type: "simple-fill", color: colors[2], outline: { width: 0 } } }] };
@@ -60,6 +61,9 @@ require([
       else if (info.type === "play-icon") iconHref += "playground.svg";
       renderer = { type: "simple", symbol: { type: "point-3d", symbolLayers: [{ type: "icon", resource: { href: iconHref }, size: 30 }] } };
     }
+    else if (info.type === "parking") {
+      renderer = { type: "simple", symbol: { type: "polygon-3d", symbolLayers: [{ type: "fill", material: { color: [0, 197, 255, 0.6] } }] } };
+    }
     else if (info.type === "building") {
       renderer = { type: "unique-value", field: "Building_ID", defaultSymbol: { type: "polygon-3d", symbolLayers: [{ type: "extrude", size: 15, material: { color: "white" } }] }, uniqueValueInfos: [{ value: 8052, symbol: { type: "polygon-3d", symbolLayers: [{ type: "extrude", size: 40, material: { color: "#2ecc71" } }] } }] };
       popupTemplate = { title: "Building Information", content: function(feature) {
@@ -85,10 +89,7 @@ require([
     map.add(layer);
   });
 
-  const view = new SceneView({
-    container: "viewDiv", map: map,
-    camera: { position: { x: 14.242, y: 57.782, z: 1200 }, tilt: 45 }
-  });
+  const view = new SceneView({ container: "viewDiv", map: map, camera: { position: { x: 14.242, y: 57.782, z: 1200 }, tilt: 45 } });
 
   view.when(() => {
     layersInfo.forEach(info => {
